1dcc3bd60321d8e70140552d53d3812491a985a0
Rouven Alexander Rieker <therouv@googlemail.com>
2015-05-30 09:08:42 +0200

    [TASK] Update to latest m2 release

--- files/reinstall.sh
+++ files/reinstall.sh
@@ -61,7 +61,7 @@ if [[ -n $SAMPLE_DATA ]]
 then
   echo "Installing sample data..."
   composer config repositories.magento composer http://packages.magento.com
-  composer require magento/sample-data:0.74.0-beta10 --dev
+  composer require magento/sample-data:0.74.0-beta11 --dev
   php -f bin/magento sampledata:install admin
   php -f bin/magento setup:upgrade
 fi
